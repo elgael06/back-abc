6730a41fac890aafac0187403f9eddb2b19808b9
gael <cgvalenzuelac@concredito.com.mx>
2020-11-08 18:43:05 -0700

    heroku.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+const PORT = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({allowedHeaders:'*'});
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
